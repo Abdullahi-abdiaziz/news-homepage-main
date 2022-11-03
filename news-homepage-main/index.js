36ce1d5c1ec5a94477ef25f51167681232c387b6
Abdullahi Abdiaziz yusuf <abdullahikawte2019@gmail.com>
2022-11-03 12:13:14 +0200

    Initial commit

---
+++ news-homepage-main/index.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector('.icon');
+const nav = document.querySelector('.navbar');
+
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('show');
+})
+
+
