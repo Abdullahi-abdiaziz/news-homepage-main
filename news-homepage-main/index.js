7f8fd9361ad50ffbe5f9c7927fdb4d972595cf66
Abdullahi Abdiaziz yusuf <abdullahikawte2019@gmail.com>
2022-11-03 12:21:40 +0200

    Challenge Updated

--- news-homepage-main/index.js
+++
@@ -1,8 +0,0 @@
-const navToggle = document.querySelector('.icon');
-const nav = document.querySelector('.navbar');
-
-navToggle.addEventListener('click', () => {
-    nav.classList.toggle('show');
-})
-
-
